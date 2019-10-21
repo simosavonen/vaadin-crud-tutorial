a2cf95ac0cb5f1aba4f3b9f039298a25ad9bec5c
Simo Savonen <sipesa@utu.fi>
2019-10-21 15:54:21 +0300

    part 3: Filtering Entities in a Grid

--- src/main/java/com/vaadin/example/MainView.java
+++ src/main/java/com/vaadin/example/MainView.java
@@ -1,11 +1,14 @@
 package com.vaadin.example;
 
-import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+
+import java.awt.*;
 
 /**
  * The main view contains a grid of Customers.
@@ -16,16 +19,24 @@ public class MainView extends VerticalLayout {
 
     private CustomerService service = CustomerService.getInstance();
     private Grid<Customer> grid = new Grid<>(Customer.class);
+    private TextField filterText = new TextField();
 
     public MainView() {
+
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.EAGER);
+        filterText.addValueChangeListener(e -> updateList());
+
         grid.setColumns("firstName", "lastName", "status" );
-        add(grid);
+
+        add(filterText, grid);
         setSizeFull();
 
         updateList();
     }
 
     public void updateList() {
-        grid.setItems(service.findAll());
+        grid.setItems(service.findAll(filterText.getValue()));
     }
 }
