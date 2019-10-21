2a3158909eea33c3898456512bd6f3a271660755
Simo Savonen <sipesa@utu.fi>
2019-10-21 15:42:36 +0300

    part 2: Listing Entities in a Grid

---
+++ src/main/java/com/vaadin/example/CustomerStatus.java
@@ -0,0 +1,5 @@
+package com.vaadin.example;
+
+public enum CustomerStatus {
+    ImportedLead, NotContacted, Contacted, Customer, ClosedLost
+}
--- src/main/java/com/vaadin/example/MainView.java
+++ src/main/java/com/vaadin/example/MainView.java
@@ -1,21 +1,31 @@
 package com.vaadin.example;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
 /**
- * The main view contains a button and a click listener.
+ * The main view contains a grid of Customers.
  */
 @Route("")
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
+    private CustomerService service = CustomerService.getInstance();
+    private Grid<Customer> grid = new Grid<>(Customer.class);
+
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        grid.setColumns("firstName", "lastName", "status" );
+        add(grid);
+        setSizeFull();
+
+        updateList();
+    }
+
+    public void updateList() {
+        grid.setItems(service.findAll());
     }
 }
